ce61d9477747dfb2969a251e5deef4c7edf2f5ce
t-coffe <tcoffe@edenschool.fr>
2023-11-13 15:19:57 +0100

    switchSucess

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ const switches = document.querySelectorAll('input[type="checkbox"]');
 
 for (let i = 0; i < switches.length; i++) {
   switches[i].addEventListener("click", ()=> {
-    switches[4].checked = !switches[4].checked
-
+    let r;
+    do {
+      r = getRandomInt(0,switches.length)
+    } while (r === i);
+    
+    switches[r].checked = !switches[r].checked;
 } );
 };
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min))+ 
+  min;
+}
